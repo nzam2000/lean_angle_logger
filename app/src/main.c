87287c49523a8fc1b1c48f3d76dc6e2896e78de1
nzamora <nzamora@mtu.edu>
2026-07-02 22:04:13 -0400

    Thread ping-pong main

--- app/src/main.c
+++ app/src/main.c
@@ -1,5 +1,36 @@
 #include <stdio.h>
+#include <zephyr/kernel.h>
 
-int main() {
-    printf("Hello world\n");
+K_MUTEX_DEFINE(m);
+
+void thread_entry(void* p1, void* p2, void* p3)
+{
+    while (1)
+    {
+        k_mutex_lock(&m, K_FOREVER);
+        printf("thread %d\n", (int)p1);
+        k_msleep(1000);
+        k_mutex_unlock(&m);
+        k_yield();
+    }
 }
+
+K_THREAD_DEFINE(
+    thread_0,
+    1024,
+    thread_entry,
+    0, NULL, NULL,
+    7,
+    0,
+    0
+    );
+
+K_THREAD_DEFINE(
+    thread_1,
+    1024,
+    thread_entry,
+    1, NULL, NULL,
+    7,
+    0,
+    0
+    );
